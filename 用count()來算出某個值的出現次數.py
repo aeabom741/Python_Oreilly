c82f89d4661fbec1ee6cf6c4588cf6b545f8d34e
aeabom741 <57891064+aeabom741@users.noreply.github.com>
2020-08-10 09:33:18 +0800

    Delete 用count()來算出某個值的出現次數.py

--- 用count()來算出某個值的出現次數.py
+++
@@ -1,7 +0,0 @@
-marxes = ['Groucho', 'Chico', 'Harpo', 'Grmmo']
-print(marxes.count('Chico'))
-.
-print(marxes.count("Bob"))
-
-snl_skit = ["cheers","cheers","cheers"]
-print(snl_skit.count("cheers"))
